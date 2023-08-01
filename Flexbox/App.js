3b840ec98174cba9743ba3dfc74d5021c4780ecb
KovacFilip <kovacfilipmh@gmail.com>
2023-08-01 12:42:35 +0200

    Played around with Flexbox

--- Flexbox/App.js
+++ Flexbox/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={{padding: 50}}>
+        <View
+            style={{
+                padding: 50,
+                flexDirection: "row",
+                width: "80%",
+                height: 300,
+                justifyContent: "space-between",
+            }}
+        >
             <View
                 style={{
-                    backgroundColor: 'red',
-                    width: 100,
-                    height: 100,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
+                    backgroundColor: "red",
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
                 <Text>1</Text>
             </View>
             <View
                 style={{
-                    backgroundColor: 'blue',
-                    width: 100,
-                    height: 100,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
+                    backgroundColor: "blue",
+                    flex: 2,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
                 <Text>2</Text>
             </View>
             <View
                 style={{
-                    backgroundColor: 'green',
-                    width: 100,
-                    height: 100,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
+                    backgroundColor: "green",
+                    flex: 3,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
                 <Text>3</Text>
             </View>
         </View>
